5ae5cfef098d7b2534fb48e05443468d9f0697ca
deval2498 <deval.minocha24@gmail.com>
2024-06-15 14:21:29 +0530

    defaultport

--- index.ts
+++ index.ts
@@ -6,7 +6,7 @@ import contactRoutes from './routes/contactRoutes';
 dotenv.config();
 
 const app = express();
-const port = process.env.PORT || 3000;
+const port = process.env.PORT || 10000;
 
 // PostgreSQL pool configuration
 const pool = new Pool({
